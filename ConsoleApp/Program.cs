060d5260c1099d643174dfc13437c060b864697f
edu551 <eduardogoncalves18@gmail.com>
2025-06-19 16:07:55 -0300

    wpf control

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+OperatorExamples();
+TrySomeLoops();
+
+void OperatorExamples()
+{
+    int width = 3;
+    width++;
+    int height = 2 + 4;
+
+    int area = width * height;
+    Console.WriteLine(area);
+
+    string result = "The area";
+    result = result + " is " + area;
+    height = 12;
+    Console.WriteLine(result);
+
+    bool truthValue = true;
+    Console.WriteLine(truthValue);
+ }
+
+void TrySomeLoops()
+{
+    int c = 0;
+    while (c < 10)
+    {
+        c = c + 1;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        c = c - 1;
+    }
+
+    Console.WriteLine(c);
+}
