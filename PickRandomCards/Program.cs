c7a3b612af9f0c3554f845241759fdb80d6e3ce7
edu551 <eduardogoncalves18@gmail.com>
2025-06-20 11:25:02 -0300

    added Unity to gitignore

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PickRandomCards;
+
+Console.Write("Enter the number of cards to pick: ");
+string line = Console.ReadLine();
+
+while (line != "q")
+{
+    if (int.TryParse(line, out int numberOfCards))
+    {
+        foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+        {
+            Console.WriteLine(card);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid value!!!!");
+    }
+
+    Console.Write("Enter the number of cards to pick: ");
+    line = Console.ReadLine();
+}
